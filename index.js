7566ec9a22c6b67bea41e3e0fd9f31685081b2cf
sinofseven <em.s.00001@gmail.com>
2019-12-23 17:18:05 +0900

    add hatena blog card

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let worker = null;
+
+try {
+  worker = browser;
+} catch {
+  worker = chrome;
+}
+
 function generate(type, title, url) {
   switch(type) {
     case 'title':
@@ -8,14 +16,37 @@ function generate(type, title, url) {
       return `<a href="${url}">${title}</a>`;
     case 'markdown':
       return `[${title}](${url})`;
+    case 'hatena-blog-card':
+      return createHatenaBlogCardLink(url, title);
   }
 }
 
+function getOptionValue(name, defaultValue) {
+  const value = localStorage.getItem(name);
+  return value == null ? defaultValue : value;
+}
+
+function createHatenaBlogCardLink(url, title) {
+  const option = {
+    width: getOptionValue("style-width", "100%"),
+    height: getOptionValue("style-height", "150px"),
+    margin: getOptionValue("margin", null),
+    'max-width': getOptionValue("max-width", "600px")
+  };
+  let style = "";
+  for (const [key, value] of Object.entries(option)) {
+    if (value == null || value === 'undefined') continue;
+    style += `${key}: ${value};`;
+  }
+  const encodedUrl = encodeURIComponent(url);
+  return `<iframe class="hatenablogcard" style="${style}" title=${JSON.stringify(title)} src="https://hatenablog-parts.com/embed?${encodedUrl}"></iframe>`
+}
+
 function copy(event) {
-  browser.tabs.query({
+  worker.tabs.query({
     active: true,
     currentWindow: true
-  }).then((list) => {
+  }, function(list) {
     const tab = list[0];
     const title = tab.title;
     const url = tab.url;
