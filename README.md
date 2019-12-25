5c41715db03117a978fb0974673ec447a6138e71
sinofseven <em.s.00001@gmail.com>
2019-12-25 14:34:55 +0900

    fix icon

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is web extension of FireFox to copy title of web page, or url.
 
-[Copy Title or Url](https://addons.mozilla.org/ja/firefox/addon/copy-title-or-url/)
+firefox: [Copy Title or Url](https://addons.mozilla.org/ja/firefox/addon/copy-title-or-url/)
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -xeu
+
 baseDir=$PWD
 
 rm -rf dist/
@@ -7,8 +9,9 @@ mkdir dist/
 
 zip \
   dist/package.zip \
-  icon.png \
-  index.css \
-  index.html \
-  index.js \
-  manifest.json
+      icon.png \
+      icon-128.png \
+      index.css \
+      index.html \
+      index.js \
+      manifest.json
